9d77659a29c99c4e7f64352ec3273fe0e1d3480c
Mark Parker <mark@parker-programs.com>
2023-06-30 20:04:37 +0200

    add warning

--- raspi_gpio/__init__.py
+++ raspi_gpio/__init__.py
@@ -2,3 +2,4 @@ try:
     from RPi.GPIO import *
 except (RuntimeError, ModuleNotFoundError, ImportError):
     from .GPIO import *
+    print('Warning: RPi.GPIO cannot be imported, using mock GPIO instead.')
