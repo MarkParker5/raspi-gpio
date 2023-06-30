9503e999d0c20735b6d59ce8e61a23ebf0b046a9
Mark Parker <mark@parker-programs.com>
2023-06-30 20:08:03 +0200

    add spidev

--- raspi_gpio/__init__.py
+++ raspi_gpio/__init__.py
@@ -1,5 +1,7 @@
 try:
     from RPi.GPIO import *
+    import spidev
 except (RuntimeError, ModuleNotFoundError, ImportError):
     from .GPIO import *
+    from .spidev import *
     print('Warning: RPi.GPIO cannot be imported, using mock GPIO instead.')
---
+++ raspi_gpio/spidev.py
@@ -0,0 +1,40 @@
+class SpiDev:
+
+    bits_per_word = 8
+    cshigh = False
+    loop = False
+    lsbfirst = False
+    max_speed_hz = 125000000
+    mode = 0
+    threewire = False
+    no_cs = False
+
+    def __init__(self, bus=0, cs=0):
+        pass
+
+    def open(self, bus, cs):
+        pass
+
+    def close(self):
+        pass
+
+    def fileno(self):
+        pass
+
+    def readbytes(self, length):
+        return []
+
+    def writebytes(self,values):
+        pass
+
+    def writebytes2(self,values):
+        pass
+
+    def xfer(self, values, speed=4096, delay=0, bits=8):
+        pass
+
+    def xfer2(self, values, speed=4096, delay=0, bits=8):
+        pass
+
+    def xfer3(self, values, speed=4096, delay=0, bits=8):
+        pass
