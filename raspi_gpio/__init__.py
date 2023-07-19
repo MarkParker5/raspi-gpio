d18dc2a3d1bf9c5ce988cdb7d96686f31aa165ab
Mark Parker <mark@parker-programs.com>
2023-07-19 14:52:53 +0200

    #1 Add `raspi_gpio.is_mocked: bool`

--- raspi_gpio/__init__.py
+++ raspi_gpio/__init__.py
@@ -1,7 +1,9 @@
 try:
     import RPi.GPIO as GPIO
     from spidev import SpiDev
+    is_mocked = False
 except (RuntimeError, ModuleNotFoundError, ImportError):
     from . import GPIO_mock as GPIO
     from .spidev_mock import SpiDev
+    is_mocked = True
     print('Warning: RPi.GPIO cannot be imported, using mock GPIO instead.')
